22cd12e75e7be2b20b4737613c6ce94cb529f912
AngelF4 <angelfhernandez71@gmail.com>
2023-09-09 16:47:42 -0600

    Sistema de Grid con Ionic

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const lista = document.getElementById('lista');
+const total = document.getElementById('total');
+
+function calcularTotal() {
+    const articuloInput = document.getElementById('articulo');
+    const precioInput = document.getElementById('precio');
+
+    const articulo = articuloInput.value;
+    const precio = parseFloat(precioInput.value);
+
+    if (articulo && !isNaN(precio)) {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${articulo}: $${precio.toFixed(2)}`;
+        lista.appendChild(listItem);
+        articuloInput.value = '';
+        precioInput.value = '';
+
+        const currentTotal = parseInt(total.textContent);
+        total.textContent = currentTotal + precio;
+    }
+}
